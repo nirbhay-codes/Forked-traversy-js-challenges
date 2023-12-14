ef454ef409af15a22f958ac434e8bcb1c360264b
Nirbhay Lourembam <32927418+nirbhay-codes@users.noreply.github.com>
2023-12-14 12:23:02 +0530

    Add solution for 01-basic-challenges-1/05-find-max-number/find-max-number.js

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,17 @@
-function findMaxNumber() {}
+/* // Solution 1
+function findMaxNumber(arr) {
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+} */
+
+// Solution 2
+function findMaxNumber(arr) {
+  return Math.max(...arr);
+}
 
 module.exports = findMaxNumber;
