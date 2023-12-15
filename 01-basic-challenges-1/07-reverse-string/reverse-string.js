6c14262f10878c1e944592a63afaa3ff5e357fc1
Nirbhay Lourembam <32927418+nirbhay-codes@users.noreply.github.com>
2023-12-15 08:45:11 +0530

    Add solution for 01-basic-challenges-1/07-reverse-string/reverse-string.js

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,15 @@
-function reverseString() {}
+/* // Solution 1
+function reverseString(str) {
+  let reverse = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverse += str[i];
+  }
+  return reverse;
+} */
+
+// Solution 2
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
 
 module.exports = reverseString;
