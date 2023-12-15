75e9407a50e9ae22eb03ff4107ead9f3e3f42a1d
Nirbhay Lourembam <32927418+nirbhay-codes@users.noreply.github.com>
2023-12-15 07:52:05 +0530

    Add solution for 01-basic-challenges-1/06-title-case/title-case-test.js

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,17 @@
-function titleCase() {}
+/* // Solution 1
+function titleCase(str) {
+  let words = str.toLowerCase().split(' ');
+
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+  }
+
+  return words.join(' ');
+} */
+
+// Solution 2
+function titleCase(str) {
+  return str.replace(/\b\w/g, (match) => match.toUpperCase());
+}
 
 module.exports = titleCase;
