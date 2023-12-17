4781a4d1e927c87ee22d24c58e0ffe663fb59c8c
Nirbhay Lourembam <32927418+nirbhay-codes@users.noreply.github.com>
2023-12-17 12:01:23 +0530

    Add notes for regex in Title case question

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -9,7 +9,13 @@ function titleCase(str) {
   return words.join(' ');
 } */
 
-// Solution 2
+/* // Solution 2
+Regex explanation:
+- \b: Word boundary assertion. This asserts that the position is at the beginning or end of a word.
+      It does not consume any characters but rather asserts the position.
+- \w: Word character shorthand. It matches any word character (alphanumeric character or underscore).
+- /g: Global flag. It means the pattern will be applied globally to the entire input string, rather than stopping after the first match.
+*/
 function titleCase(str) {
   return str.replace(/\b\w/g, (match) => match.toUpperCase());
 }
