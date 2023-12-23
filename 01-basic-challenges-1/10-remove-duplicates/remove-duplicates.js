66a21a6678b52d7a91e83bf1c64f342de786948c
Nirbhay Lourembam <32927418+nirbhay-codes@users.noreply.github.com>
2023-12-23 22:22:39 +0530

    Add solution 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,15 @@
-function removeDuplicates() {}
+/* function removeDuplicates(arr) {
+  let uniqueResult = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!uniqueResult.includes(arr[i])) {
+      uniqueResult.push(arr[i]);
+    }
+  }
+  return uniqueResult;
+} */
+
+function removeDuplicates(arr) {
+  return Array.from(new Set(arr));
+}
 
 module.exports = removeDuplicates;
